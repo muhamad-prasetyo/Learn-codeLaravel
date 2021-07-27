e04cdb7c11dfa8bc423e384b81ac646d0e8f0d10
Muhamad Prasetyo <muhamad.mprasetyo@gmail.com>
2021-07-27 12:36:59 +0700

    membuat contoh route di dalme frefix ada frefix

--- Route-laravel/routing-group.php
+++ Route-laravel/routing-group.php
@@ -2,7 +2,6 @@
 
     // * ROUTE GROUP UNTUK MEMPERMUDAH DAN MEMPERSINGKAT PENULISAN CODE PADA ROUTE,
     // * JIKA ROUTE YANG AKAN DIBUAT SAMA DAN DIGUNAKAN UNTUK HAL YANG DIAKSES SAMA BEDA PREFIX URL BELAKANG SAJA 
-
     // todo ||  EXAMPLE PEMBUATAN ROUTE PROFIL USER 
 
     // * EXAMPLE : 
@@ -24,3 +23,32 @@
 
     // ? dan untuk memanggilnya di url browser : 
     // * example: evaling.com/user/nama route yang dipanggil
+
+
+
+    // * ATAU  MEMBUAT ROUTE DI DALAM ROUTE GROUP 
+    // * EXAMPLE : Pembuatan Route Account Profil
+    Route::prefix('account')->group(function() {
+        // definiskan Route
+        Route::prefix('setting')->group(function() {
+            Route::get('/change-password', function() {
+                return 'Change-password';
+            });
+
+            Route::get('/profil', function() {
+                return 'Profil';
+            });
+
+            Route::get('/photo', function() {
+                return 'Photo';
+            }); 
+        });
+
+        // diluar prefix setting 
+        Route::get('follower', function() {
+            return 'Follower';
+        });
+    });
+    // ? dan untuk memanggilnya di url browser : 
+    // * example: evaling.com/account/setting/nama yang ingin dipilih
+
