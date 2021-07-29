9e5bdc7ef6ee2cc48a17966433471811cffc5ee1
Muhamad Prasetyo <muhamad.mprasetyo@gmail.com>
2021-07-30 01:57:38 +0700

    memperjelas arti dan code

--- migration/membuat-migration.php
+++ migration/membuat-migration.php
@@ -11,7 +11,7 @@
     ####################################################################################################################
 
     // * SETELAH KITA BUAT SEBUAH TABLE DENGAN MENGGUNAKAN MIGRATION TERSEBUT. MAKA LARAVEL AKAN MEMBUATKAN SEBUAH FILE DIDALAM 
-    // TODO FOLDER database/migration/namaFile.
+    // Todo FOLDER database/migration/namaFile.
     // * LALU DIDALAM FILE YANG TELAH DIBUA DIDALAMNYA KITA BISA ATUR ATAU BUAT FIELD NYA APA SAJA YG AKAN MAU DIBUAT 
     // * CONTOH DIDALAM FILE TERSEBUT, TERDAPAT METHOD BERNAMA FUNCTION UP(), DIDALAM METHOD TERSEBUT KITA BISA BUAT SEBUAH FIELD YANG AKAN INGIN BUAT .
     
@@ -24,5 +24,25 @@
         });
     }
 
+    // ! NOTE :
+    // * Default bawaan isi methodnya seperti diatas. Jika kita ingin menambahkan field sesuai kebutuhan kita maka kita bisa menambahkan sebuah field ditengah-tengah field default / bawaannya tersebut.
 
-    
+    EXAMPLE : 
+    public function up()
+    {
+        Schema::create('users', function(Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 50);
+            $table->string('email', 50)->unique(); // FUNGSI UNIQUE INI BERGUNA UNTUK MENYURUH USER MEMASUKAN EMAIL DENGAN UNIK
+            $table->string('password');
+            $table->string('address')->nullable()->default(null);
+            $table->timestamps();
+        });
+    }
+
+    // FUNGSI NULLABLE() YAITU UNTUK JIKA USER TIDAK MENGISIKAN FORM MAKA KOSONG.
+
+
+    // * SETELAH SELESAI KITA MEMBUAT TAMBAHAN FIELDNYA LALU KITA JALANKAN MIGRATIONNYA DENGAN CARA BERIKUT.
+    // * EXAMPLE : 
+    php artisan migrate 
